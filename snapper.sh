f360b22301deffbe33dfa2f14bd934f7cdb79bd8
Francis Tseng <f+accounts@frnsys.com>
2018-10-26 12:12:10 -0400

    updated snapper

--- snapper.sh
+++ snapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-    fswebcam -d /dev/video1 -r 1920x1080 -F 1 -S 32 --no-banner /tmp/output.jpeg
+    fswebcam -d /dev/video0 -r 1920x1080 -F 1 -S 32 --no-banner --set brightness=30% --jpeg 100 /tmp/output.jpeg
     dt=$(date +"%Y-%m-%d_%H-%M-%S")
     cp /tmp/output.jpeg "plants/${dt}.jpg"
     scp /tmp/output.jpeg ftseng@starbase.in:/srv/frnsys/plants.jpg
